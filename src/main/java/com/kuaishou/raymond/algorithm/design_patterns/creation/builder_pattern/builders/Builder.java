6da706e7c96f2ce1b8cae64d35851f94e2ec4471
zhaolei09 <zhaolei09@kuaishou.com>
2022-09-29 14:31:52 +0800

    add builder pattern

---
+++ src/main/java/com/kuaishou/raymond/algorithm/design_patterns/creation/builder_pattern/builders/Builder.java
@@ -0,0 +1,51 @@
+package com.kuaishou.raymond.algorithm.design_patterns.creation.builder_pattern.builders;
+
+import com.kuaishou.raymond.algorithm.design_patterns.creation.builder_pattern.cars.CarType;
+import com.kuaishou.raymond.algorithm.design_patterns.creation.builder_pattern.components.Engine;
+import com.kuaishou.raymond.algorithm.design_patterns.creation.builder_pattern.components.GPSNavigator;
+import com.kuaishou.raymond.algorithm.design_patterns.creation.builder_pattern.components.Transmission;
+import com.kuaishou.raymond.algorithm.design_patterns.creation.builder_pattern.components.TripComputer;
+
+/**
+ * @author raymond <zhaolei09@kuaishou.com>
+ * created on 2022-09-28 20:29
+ */
+public interface Builder {
+
+    /**
+     * 设置汽车类型
+     * @param type
+     */
+    void setCarType(CarType type);
+
+    /**
+     * 汽车座位数
+     * @param seats
+     */
+    void setSeats(int seats);
+
+    /**
+     * 汽车引擎
+     * @param engine
+     */
+    void setEngine(Engine engine);
+
+    /**
+     * 汽车发射器
+     * @param transmission
+     */
+    void setTransmission(Transmission transmission);
+
+    /**
+     * 里程记录仪
+     * @param tripComputer
+     */
+    void setTripComputer(TripComputer tripComputer);
+
+    /**
+     * GPS 导航
+     * @param gpsNavigator
+     */
+    void setGPSNavigator(GPSNavigator gpsNavigator);
+
+}

2c24fca275b242d24f38200c18a5291afee6be1e
raymond <qq740567396@gmail.com>
2023-02-04 11:14:31 +0800

    StO:day5

--- src/main/java/com/kuaishou/raymond/algorithm/swordtooffer/day5_search_medium/P4.java
+++ src/main/java/com/kuaishou/raymond/algorithm/swordtooffer/day5_search_medium/P4.java
@@ -7,18 +7,78 @@ package com.kuaishou.raymond.algorithm.swordtooffer.day5_search_medium;
  * 输入这样的一个二维数组和一个整数，判断数组中是否含有该整数。
  * matrix=
  * [
- *   [1,   4,  7, 11, 15],
- *   [2,   5,  8, 12, 19],
- *   [3,   6,  9, 16, 22],
- *   [10, 13, 14, 17, 24],
- *   [18, 21, 23, 26, 30]
+ * [1,   4,  7, 11, 15],
+ * [2,   5,  8, 12, 19],
+ * [3,   6,  9, 16, 22],
+ * [10, 13, 14, 17, 24],
+ * [18, 21, 23, 26, 30]
  * ]
- *
  */
 public class P4 {
 
-    public boolean findNumberIn2DArray(int[][] matrix, int target) {
+    public static void main(String[] args) {
+        int[][] matrix = {
+                {1, 4, 7, 11, 15},
+                {2, 5, 8, 12, 19},
+                {3, 6, 9, 16, 22},
+                {10, 13, 14, 17, 24},
+                {18, 21, 23, 26, 30}
+        };
+        P4 p4 = new P4();
+        System.out.println("p4.findNumberIn2DArray(matrix, 9) = " + p4.findNumberIn2DArray(matrix, 133));
+        System.out.println("p4.findNumberIn2DArray2(matrix, 9) = " + p4.findNumberIn2DArray2(matrix, 133));
+    }
+
+    /**
+     * 大上小右
+     */
+    public boolean findNumberIn2DArray2(int[][] matrix, int target) {
+        // 从左下角开始向上，向右搜索。
+        int i = matrix.length - 1;
+        int j = 0;
+        while (i >= 0 && j < matrix[0].length) {
+            if (matrix[i][j] < target) {
+                // 如果当前值比 target 小，那么它上边的也比 target 小，排除当前列，向右寻找。
+                j++;
+            } else if (matrix[i][j] > target) {
+                // 如果当前值比 target 大，那么它右边的也比 target 大，排除当前行，向上寻找、
+                i--;
+            } else {
+                return true;
+            }
+        }
         return false;
+    }
+
+    /**
+     * 对每个行/列进行二分查找
+     * 时间复杂度 O(mlgn) 或 O(nlgm)
+     * @param matrix
+     * @param target
+     * @return
+     */
+    public boolean findNumberIn2DArray(int[][] matrix, int target) {
+        for (int[] row : matrix) {
+            if (binarySearch(row, target) != -1) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private int binarySearch(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        while (left <= right) {
+            int middle = (left + right) >>> 1;
+            if (nums[middle] < target) {
+                left = middle + 1;
+            } else if (nums[middle] > target) {
+                right = middle - 1;
+            } else {
+                return middle;
+            }
+        }
+        return -1;
     }
 
 }
--- src/main/java/com/kuaishou/raymond/algorithm/swordtooffer/day5_search_medium/P50.java
+++ src/main/java/com/kuaishou/raymond/algorithm/swordtooffer/day5_search_medium/P50.java
@@ -1,7 +1,50 @@
 package com.kuaishou.raymond.algorithm.swordtooffer.day5_search_medium;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * 第一个只出现一次的数字
+ * 在字符串 s 中找出第一个只出现一次的字符。如果没有，返回一个单空格。 s 只包含小写字母。
  */
 public class P50 {
+
+    public static void main(String[] args) {
+        String s = "abaccdeff";
+        String s1 = "";
+        String s2 = "leetcode";
+        P50 p50 = new P50();
+        System.out.println("p50.firstUniqChar(s) = " + p50.firstUniqChar(s));
+        System.out.println("p50.firstUniqChar(s) = " + p50.firstUniqChar(s1));
+        System.out.println("p50.firstUniqChar(s) = " + p50.firstUniqChar(s2));
+    }
+
+    public char firstUniqChar(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0) + 1);
+        }
+        for (int i = 0; i < s.length(); i++) {
+            if (map.get(s.charAt(i)) == 1) {
+                return s.charAt(i);
+            }
+        }
+        return ' ';
+    }
+
+    public char firstUniqCharOfOrderedString(String s) {
+        int[] helper = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            helper[c - 'a']++;
+        }
+
+        for (int i = 0; i < helper.length; i++) {
+            if (helper[i] == 1) {
+                return (char) (i + 'a');
+            }
+        }
+        return ' ';
+    }
+
 }

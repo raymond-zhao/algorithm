0deed6e572600f55e71dcde2ac85b4d98a9593ca
raymond <qq740567396@gmail.com>
2023-02-05 15:01:55 +0800

    StO:day6

---
+++ src/main/java/com/kuaishou/raymond/algorithm/swordtooffer/TreeNode.java
@@ -0,0 +1,28 @@
+package com.kuaishou.raymond.algorithm.swordtooffer;
+
+public class TreeNode {
+    public int val;
+
+    public TreeNode left;
+
+    public TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+
+    public static TreeNode defaultTree() {
+        TreeNode root = new TreeNode(3);
+        TreeNode node1 = new TreeNode(9);
+        TreeNode node2 = new TreeNode(20);
+        TreeNode node3 = new TreeNode(15);
+        TreeNode node4 = new TreeNode(7);
+
+        root.left = node1;
+        root.right = node2;
+        node2.left = node3;
+        node2.right = node4;
+
+        return root;
+    }
+}
--- src/main/java/com/kuaishou/raymond/algorithm/swordtooffer/day6_search_and_backtrace/P32_1.java
+++ src/main/java/com/kuaishou/raymond/algorithm/swordtooffer/day6_search_and_backtrace/P32_1.java
@@ -1,4 +1,49 @@
 package com.kuaishou.raymond.algorithm.swordtooffer.day6_search_and_backtrace;
 
+import com.kuaishou.raymond.algorithm.swordtooffer.TreeNode;
+
+import java.util.*;
+
+/**
+ * 从上到下打印出二叉树的每个节点，同一层的节点按照从左到右的顺序打印。
+ *     3
+ *    / \
+ *   9  20
+ *     /  \
+ *    15   7
+ */
 public class P32_1 {
+
+    public static void main(String[] args) {
+        P32_1 p321 = new P32_1();
+        System.out.println("Arrays.toString(p321.levelOrder(TreeNode.defaultTree())) = " + Arrays.toString(p321.levelOrder(TreeNode.defaultTree())));
+    }
+
+    public int[] levelOrder(TreeNode root) {
+        if (root == null) {
+            return new int[0];
+        }
+
+        List<TreeNode> treeNodeList = new ArrayList<>();
+        Queue<TreeNode> queue = new ArrayDeque<>();
+        queue.offer(root);
+
+        while (!queue.isEmpty()) {
+            TreeNode currentTreeNode = queue.poll();
+            treeNodeList.add(currentTreeNode);
+            if (currentTreeNode.left != null) {
+                queue.offer(currentTreeNode.left);
+            }
+            if (currentTreeNode.right != null) {
+                queue.offer(currentTreeNode.right);
+            }
+        }
+
+        int[] res = new int[treeNodeList.size()];
+        for (int i = 0; i < res.length; i++) {
+            res[i] = treeNodeList.get(i).val;
+        }
+        return res;
+    }
+
 }

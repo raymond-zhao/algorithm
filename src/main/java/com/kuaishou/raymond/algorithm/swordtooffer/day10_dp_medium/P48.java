3dffeb7bed124d557cd76cd8d993856bcee34d00
raymond <qq740567396@gmail.com>
2023-02-07 09:13:47 +0800

    StO:day9-11题目

--- src/main/java/com/kuaishou/raymond/algorithm/swordtooffer/day10_dp_medium/P48.java
+++ src/main/java/com/kuaishou/raymond/algorithm/swordtooffer/day10_dp_medium/P48.java
@@ -1,4 +1,23 @@
 package com.kuaishou.raymond.algorithm.swordtooffer.day10_dp_medium;
 
+/**
+ * <a href="https://leetcode.cn/problems/zui-chang-bu-han-zhong-fu-zi-fu-de-zi-zi-fu-chuan-lcof/">最长不含重复字符的子字符串
+ * 请从字符串中找出一个最长的不包含重复字符的子字符串，计算该最长子字符串的长度。
+ * 输入: "abcabcbb"
+ * 输出: 3
+ * 解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3。
+ * 输入: "bbbbb"
+ * 输出: 1
+ * 解释: 因为无重复字符的最长子串是 "b"，所以其长度为 1。
+ * 输入: "pwwkew"
+ * 输出: 3
+ * 解释: 因为无重复字符的最长子串是"wke"，所以其长度为 3。请注意，你的答案必须是 子串 的长度，"pwke"是一个子序列，不是子串。
+ * s.length <= 40000
+ */
 public class P48 {
+
+    public int lengthOfLongestSubstring(String s) {
+
+    }
+
 }
--- src/main/java/com/kuaishou/raymond/algorithm/swordtooffer/day9_dp_medium/P42.java
+++ src/main/java/com/kuaishou/raymond/algorithm/swordtooffer/day9_dp_medium/P42.java
@@ -1,4 +1,18 @@
 package com.kuaishou.raymond.algorithm.swordtooffer.day9_dp_medium;
 
+/**
+ * 连续子数组的最大和
+ * 输入一个整形数组，数组中的一个或多个整数组成一个子数组，求所有子数组和的最大和。
+ * 要求时间复杂为 O(n).
+ */
 public class P42 {
+
+    public static void main(String[] args) {
+        int[] nums = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+
+    }
+
+    public int maxSubArray(int[] nums) {
+        return 0;
+    }
 }

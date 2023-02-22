d7bb82797ba9d777acbf3571f0a2bbe42ec51161
zhaolei09 <zhaolei09@kuaishou.com>
2023-02-22 10:13:23 +0800

    day29unfinished

--- src/main/java/com/kuaishou/raymond/algorithm/swordtooffer/day29_dp_hard_star/P19_star.java
+++ src/main/java/com/kuaishou/raymond/algorithm/swordtooffer/day29_dp_hard_star/P19_star.java
@@ -3,10 +3,10 @@ package com.kuaishou.raymond.algorithm.swordtooffer.day29_dp_hard_star;
 /**
  * 正则表达式匹配
  */
-public class P19 {
+public class P19_star {
 
     public static void main(String[] args) {
-        P19 p19 = new P19();
+        P19_star p19 = new P19_star();
         String s = "aaa";
         String p1 = "a.a";
         String p2 = "ab*ac*a";
@@ -29,14 +29,15 @@ public class P19 {
      * 如果 p[j-1..j] 匹配了 0 次，即「某个普通字符/. + *」的组合匹配了 0 次，f[i][j]=f[i][j-2];
      * 如果 p[j-1..j] 匹配了 1 次或多次，f[i][j]=f[i-N][j-2]... 这样编程是非常困难的。
      * 所以转换思路：字符加 * 号的组合本质上有两种情况，
-     * -「字符+*」组合匹配了 s[i], 则丢弃 s[i], 继续匹配 s[i-1], s[i-2], s[i-3]...
-     * -「字符+*」组合没有匹配 s[i]，则丢弃「字符+*」组合，则丢弃这个组合，使用 p[0..j-2] 继续匹配。
+     * -「字符+*」组合匹配了 s[i], 则丢弃 s[i], 继续匹配 s[i-1], f[i][j]=f[i-1][j] || f[i][j]=f[i][j-2]，如果 s[i] 与 p[j-1] 匹配；
+     * -「字符+*」组合没有匹配 s[i]，则丢弃「字符+*」组合，则丢弃这个组合，使用 p[0..j-2] 继续匹配。f[i][j]=f[i][j-2]，如果 s[i] 与 p[j-1] 不匹配。
      * ---
      * 一个特例，如果 p=.*，则可以匹配任意 s。
      */
     public boolean isMatch(String s, String p) {
         int m = s.length();
         int n = p.length();
+
         boolean[][] f = new boolean[m + 1][n + 1];
         f[0][0] = true;
 

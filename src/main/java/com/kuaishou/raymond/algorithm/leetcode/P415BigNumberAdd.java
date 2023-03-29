4014a76c290e29c7a595d631b59dc302fe39c574
zhaolei09 <zhaolei09@kuaishou.com>
2023-03-29 21:14:36 +0800

    添加ChatGPT搜到的PDD编程题

--- src/main/java/com/kuaishou/raymond/algorithm/leetcode/P415BigNumberAdd.java
+++ src/main/java/com/kuaishou/raymond/algorithm/leetcode/P415BigNumberAdd.java
@@ -1,5 +1,9 @@
 package com.kuaishou.raymond.algorithm.leetcode;
 
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.util.function.BiFunction;
+
 /**
  * @author raymond <zhaolei09@kuaishou.com>
  * created on 2023-03-29 12:55
@@ -11,8 +15,30 @@ public class P415BigNumberAdd {
         String num1 = "11";
         String num2 = "199";
         String num3 = "9999999999999999";
-        System.out.println("addStrings(num1, num2) = " + addStrings(num1, num2));
+        String num4 = "834719934878219838199128317837821319371893812319831";
+        String num5 = "7412834321642714621199128317837821319371893812319831834719934878219838199128317837821319371893812319831";
+
+        BigInteger bigInteger4 = new BigInteger(num4);
+        BigInteger bigInteger5 = new BigInteger(num5);
+        BigDecimal bigDecimal4 = new BigDecimal(num4);
+        BigDecimal bigDecimal5 = new BigDecimal(num5);
+
+        System.out.println("bigDecimal4.add(bigDecimal5) = " + bigDecimal4.add(bigDecimal5));
+
+        System.out.println("bigInteger4.add(bigInteger5) = " + bigInteger4.add(bigInteger5));
+        System.out.println("bigInteger4.subtract(bigInteger5) = " + bigInteger4.subtract(bigInteger5));
+        System.out.println("bigInteger4.multiply(bigInteger5) = " + bigInteger4.multiply(bigInteger5));
+        System.out.println("bigInteger5.divide(bigInteger4) = " + bigInteger5.divide(bigInteger4));
+
+        System.out.println("addStrings(num1, num2) = " + addStrings(num4, num5));
         System.out.println("addStrings(num1, num3) = " + addStrings(num1, num3));
+
+    }
+
+    public String bigIntegerOperation(String num1, String num2, BiFunction<BigInteger, BigInteger, String> function) {
+        BigInteger n1 = new BigInteger(num1);
+        BigInteger n2 = new BigInteger(num2);
+        return function.apply(n1, n2);
     }
 
     /***

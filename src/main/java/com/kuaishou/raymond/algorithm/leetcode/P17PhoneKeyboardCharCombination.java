cad2405729abc41ee925528ef3129014b2b2e3da
raymond <qq740567396@gmail.com>
2023-04-12 08:20:51 +0800

    回溯-组合

---
+++ src/main/java/com/kuaishou/raymond/algorithm/leetcode/P17PhoneKeyboardCharCombination.java
@@ -0,0 +1,64 @@
+package com.kuaishou.raymond.algorithm.leetcode;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author raymond <zhaolei09@kuaishou.com>
+ * created on 2023-04-11 15:03
+ * 给定一个仅包含数字 2~9 的字符串，返回所有它能表示的字母组合。答案可以任意顺序返回。
+ * 扫描数字组合，
+ */
+public class P17PhoneKeyboardCharCombination {
+
+    public static void main(String[] args) {
+        P17PhoneKeyboardCharCombination p17 = new P17PhoneKeyboardCharCombination();
+        System.out.println("p17.combination(\"23\") = " + p17.combination("23"));
+    }
+
+    protected static final Map<Character, String> MAP = new HashMap<>();
+
+    static {
+        MAP.put('2', "abc");
+        MAP.put('3', "def");
+        MAP.put('4', "ghi");
+        MAP.put('5', "jkl");
+        MAP.put('6', "mno");
+        MAP.put('7', "pqrs");
+        MAP.put('8', "tuv");
+        MAP.put('9', "wxyz");
+        MAP.put('0', "");
+        MAP.put('1', "");
+    }
+
+    private List<String> data;
+
+    private StringBuilder builder;
+
+    public List<String> combination(String digits) {
+        data = new ArrayList<>();
+        builder = new StringBuilder();
+
+        char[] digitChars = digits.toCharArray();
+
+        backtrack(digitChars, 0);
+
+        return data;
+    }
+
+    private void backtrack(char[] digits, int charIdx) {
+        if (charIdx == digits.length) {
+            data.add(builder.toString());
+            return;
+        }
+        // 罗列所有选择
+        String s = MAP.get(digits[charIdx]);
+        for (int i = 0; i < s.length(); i++) {
+            builder.append(s.charAt(i));
+            backtrack(digits, charIdx + 1);
+            builder.deleteCharAt(builder.length() - 1);
+        }
+    }
+}
---
+++ src/main/java/com/kuaishou/raymond/algorithm/leetcode/P40Combination2.java
@@ -0,0 +1,62 @@
+package com.kuaishou.raymond.algorithm.leetcode;
+
+import java.util.*;
+
+/**
+ * @author raymond <zhaolei09@kuaishou.com>
+ * created on 2023-04-11 14:44
+ */
+public class P40Combination2 {
+
+    public static void main(String[] args) {
+        P40Combination2 p40 = new P40Combination2();
+        int[] candidates = {2, 5, 2, 1, 2};
+        int target = 5;
+        System.out.println("p39.combination(candidates, target) = " + p40.combination(candidates, target));
+    }
+
+    private List<List<Integer>> data;
+
+    private Deque<Integer> path;
+
+    private boolean[] used;
+
+    public List<List<Integer>> combination(int[] candidates, int target) {
+        data = new ArrayList<>();
+        path = new ArrayDeque<>();
+        used = new boolean[candidates.length];
+
+        Arrays.sort(candidates);
+        backtrack(candidates, target, 0, 0);
+
+        return data;
+    }
+
+    private void backtrack(int[] candidates, int target, int sum, int idx) {
+        if (sum == target) {
+            data.add(new ArrayList<>(path));
+            return;
+        }
+        if (sum > target) {
+            return;
+        }
+        // 罗列所有选择
+        for (int i = idx; i < candidates.length; i++) {
+            // 去重
+            if (i > idx && candidates[i] == candidates[i - 1] && !used[i - 1]) {
+                continue;
+            }
+            // 做出选择
+            sum += candidates[i];
+            path.addLast(candidates[i]);
+            used[i] = true;
+            // 回溯
+            backtrack(candidates, target, sum, i + 1);
+            // 撤销选择
+            sum -= candidates[i];
+            path.removeLast();
+            used[i] = false;
+        }
+    }
+
+}

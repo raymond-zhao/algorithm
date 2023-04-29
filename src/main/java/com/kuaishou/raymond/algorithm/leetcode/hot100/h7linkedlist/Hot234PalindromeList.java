b01442fb4ce0ce936a9817a8c3f26bc3013f2376
raymond <qq740567396@gmail.com>
2023-04-29 23:00:16 +0800

    回文链表

---
+++ src/main/java/com/kuaishou/raymond/algorithm/leetcode/hot100/h7linkedlist/Hot234PalindromeList.java
@@ -0,0 +1,100 @@
+package com.kuaishou.raymond.algorithm.leetcode.hot100.h7linkedlist;
+
+import com.kuaishou.raymond.algorithm.leetcode.ListNode;
+import com.kuaishou.raymond.algorithm.utils.AlgoUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * <a href="https://leetcode.cn/problems/palindrome-linked-list/?envType=study-plan-v2&id=top-100-liked">...</a>
+ * 234. 回文链表
+ */
+public class Hot234PalindromeList {
+
+    public static void main(String[] args) {
+        ListNode head = AlgoUtils.buildLinkedList(AlgoUtils.toArray("[1,2,2,1]"));
+        Hot234PalindromeList hot = new Hot234PalindromeList();
+        hot.isPalindrome(head);
+    }
+
+    /**
+     * 1. 寻找链表中点
+     * 2. 翻转后半段链表
+     * 3. 从链表头部 head 开始与反转后的后半段链表作比较
+     */
+    public boolean isPalindrome(ListNode head) {
+        if (head == null) {
+            return false;
+        }
+        // 1. 获取链表中间结点
+        ListNode middleNode = getMiddleNode(head);
+        // 翻转后半段链表
+        ListNode latterNode = reverseList(middleNode);
+        // 断开链表
+
+        // 开始比较
+        while (head != null && latterNode != null) {
+            if (head.val != latterNode.val) {
+                return false;
+            }
+            head = head.next;
+            latterNode = latterNode.next;
+        }
+        return true;
+    }
+
+    public ListNode reverseList(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+        ListNode previous = null;
+        while (head != null) {
+            ListNode next = head.next;
+            head.next = previous;
+            previous = head;
+            head = next;
+        }
+        return previous;
+    }
+
+    private ListNode getMiddleNode(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+
+    public boolean isPalindromeII(ListNode head) {
+        if (head == null) {
+            return false;
+        }
+        List<Integer> data = new ArrayList<>();
+        while (head != null) {
+            data.add(head.val);
+            head = head.next;
+        }
+
+        return isPalindrome(data);
+    }
+
+    private boolean isPalindrome(List<Integer> data) {
+        int left = 0;
+        int right = data.size() - 1;
+        while (left < right) {
+            if (data.get(left) != data.get(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+}

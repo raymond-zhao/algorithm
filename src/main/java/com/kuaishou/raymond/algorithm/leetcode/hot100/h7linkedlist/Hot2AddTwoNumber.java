c8124f274a4c71e2d8992d61fa3ffa1a3a0e97d0
raymond <qq740567396@gmail.com>
2023-04-29 23:52:29 +0800

    两数相加

---
+++ src/main/java/com/kuaishou/raymond/algorithm/leetcode/hot100/h7linkedlist/Hot2AddTwoNumber.java
@@ -0,0 +1,40 @@
+package com.kuaishou.raymond.algorithm.leetcode.hot100.h7linkedlist;
+
+import com.kuaishou.raymond.algorithm.leetcode.ListNode;
+
+/**
+ * <a href="https://leetcode.cn/problems/add-two-numbers/?envType=study-plan-v2&id=top-100-liked">...</a>
+ * 2. 两数相加
+ */
+public class Hot2AddTwoNumber {
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummyHead = new ListNode(-1);
+        ListNode node = dummyHead;
+        int carry = 0;
+
+        while (l1 != null || l2 != null) {
+            int num1 = l1 != null ? l1.val : 0;
+            int num2 = l2 != null ? l2.val : 0;
+            int sum = num1 + num2 + carry;
+
+            node.next = new ListNode(sum % 10);
+            carry = sum / 10;
+
+            if (l1 != null) {
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                l2 = l2.next;
+            }
+            node = node.next;
+        }
+
+        if (carry != 0) {
+            node.next = new ListNode(carry);
+        }
+
+        return dummyHead.next;
+    }
+
+}

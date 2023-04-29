7d73c8a0025fbe8b699acab82127133973601e75
raymond <qq740567396@gmail.com>
2023-04-29 22:24:24 +0800

    反转链表

---
+++ src/main/java/com/kuaishou/raymond/algorithm/leetcode/hot100/h7linkedlist/Hot206ReverseList.java
@@ -0,0 +1,37 @@
+package com.kuaishou.raymond.algorithm.leetcode.hot100.h7linkedlist;
+
+import com.kuaishou.raymond.algorithm.leetcode.ListNode;
+
+/**
+ * <a href="https://leetcode.cn/problems/reverse-linked-list/?envType=study-plan-v2&id=top-100-liked">...</a>
+ * 206. 反转链表
+ */
+public class Hot206ReverseList {
+
+    /**
+     * 迭代
+     */
+    public ListNode reverseList(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+        ListNode previous = null;
+        while (head != null) {
+            ListNode next = head.next;
+            head.next = previous;
+            previous = head;
+            head = next;
+        }
+        return previous;
+    }
+
+    public ListNode reverseListRecursive(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode node = reverseListRecursive(head.next);
+        head.next.next = head;
+        head.next = null;
+        return node;
+    }
+}

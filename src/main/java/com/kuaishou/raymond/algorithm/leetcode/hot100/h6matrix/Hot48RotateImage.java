6cb898220cb58f4fec162842bbf869a0b1d60485
raymond <qq740567396@gmail.com>
2023-04-29 20:40:06 +0800

    旋转图像

---
+++ src/main/java/com/kuaishou/raymond/algorithm/leetcode/hot100/h6matrix/Hot48RotateImage.java
@@ -0,0 +1,27 @@
+package com.kuaishou.raymond.algorithm.leetcode.hot100.h6matrix;
+
+/**
+ * <a href="https://leetcode.cn/problems/rotate-image/?envType=study-plan-v2&id=top-100-liked">...</a>
+ * 48. 旋转图像
+ */
+public class Hot48RotateImage {
+
+    /**
+     * 转移方程：newMatrix[i][j] = matrix[j][n - i - 1]
+     */
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+        int[][] newMatrix = new int[n][n];
+
+        for (int row = 0; row < n; row++) {
+            for (int col = 0; col < n; col++) {
+                newMatrix[col][n - row - 1] = matrix[row][col];
+            }
+        }
+
+        for (int row = 0; row < n; row++) {
+            System.arraycopy(newMatrix[row], 0, matrix[row], 0, n);
+        }
+    }
+
+}

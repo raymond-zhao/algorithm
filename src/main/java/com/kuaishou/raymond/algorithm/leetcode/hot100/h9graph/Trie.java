ccd7e6d5cdf847652ee28eccee35cb356c048aa9
raymond <qq740567396@gmail.com>
2023-05-01 22:28:09 +0800

    前缀树

---
+++ src/main/java/com/kuaishou/raymond/algorithm/leetcode/hot100/h9graph/Trie.java
@@ -0,0 +1,63 @@
+package com.kuaishou.raymond.algorithm.leetcode.hot100.h9graph;
+
+/**
+ * Author: raymond
+ * CreateTime: 2023/5/1 20:50
+ * 题目：<a href="https://leetcode.cn/problems/implement-trie-prefix-tree/?envType=study-plan-v2&id=top-100-liked">208. 实现 Trie (前缀树)</a>
+ */
+public class Trie {
+
+    public static void main(String[] args) {
+        Trie trie = new Trie();
+        trie.insert("apple");
+        trie.search("apple");
+        trie.search("app");
+        trie.insert("app");
+        trie.search("app");
+    }
+
+    private final Trie[] children;
+
+    private boolean isEnd;
+
+    public Trie() {
+        children = new Trie[26];
+        isEnd = false;
+    }
+
+    public void insert(String word) {
+        Trie node = this;
+        for (int i = 0; i < word.length(); i++) {
+            char ch = word.charAt(i);
+            int index = ch - 'a';
+            if (node.children[index] == null) {
+                node.children[index] = new Trie();
+            }
+            node = node.children[index];
+        }
+        node.isEnd = true;
+    }
+
+    public boolean search(String word) {
+        Trie node = searchPrefix(word);
+        return node != null && node.isEnd;
+    }
+
+    public boolean startsWith(String prefix) {
+        return searchPrefix(prefix) != null;
+    }
+
+    private Trie searchPrefix(String prefix) {
+        Trie node = this;
+        for (int i = 0; i < prefix.length(); i++) {
+            char ch = prefix.charAt(i);
+            int index = ch - 'a';
+            if (node.children[index] == null) {
+                return null;
+            }
+            node = node.children[index];
+        }
+        return node;
+    }
+
+}

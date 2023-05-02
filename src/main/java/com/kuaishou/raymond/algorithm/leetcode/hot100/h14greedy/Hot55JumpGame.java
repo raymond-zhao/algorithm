6d29371defd137ab9bf72f4fc5ce0ab525ce2d48
raymond <qq740567396@gmail.com>
2023-05-02 23:47:24 +0800

    跳跃游戏

--- src/main/java/com/kuaishou/raymond/algorithm/leetcode/hot100/h14greedy/Hot55JumpGame.java
+++ src/main/java/com/kuaishou/raymond/algorithm/leetcode/hot100/h14greedy/Hot55JumpGame.java
@@ -1,13 +1,19 @@
-package com.kuaishou.raymond.algorithm.leetcode.all;
+package com.kuaishou.raymond.algorithm.leetcode.hot100.h14greedy;
 
-public class P55JumpGameI {
+/**
+ * Author: raymond
+ * CreateTime: 2023/5/2 23:33
+ * 题目：<a href="https://leetcode.cn/problems/jump-game/?envType=study-plan-v2&id=top-100-liked">55. 跳跃游戏</a>
+ */
+public class Hot55JumpGame {
 
     public boolean canJump(int[] nums) {
         int maxPosition = 0;
         for (int currentPosition = 0; currentPosition < nums.length; currentPosition++) {
             if (currentPosition <= maxPosition) {
                 // 如果当前位置还在之前的跳跃距离之内，则计算下一跳的最远距离。
-                maxPosition = Math.max(maxPosition, currentPosition + nums[currentPosition]);
+                int jumpDistance = nums[currentPosition];
+                maxPosition = Math.max(maxPosition, currentPosition + jumpDistance);
                 if (maxPosition >= nums.length - 1) {
                     return true;
                 }
@@ -15,4 +21,5 @@ public class P55JumpGameI {
         }
         return false;
     }
+
 }

229d897cfb469e5f2f1e94f7f61a62539816ac1a
raymond <qq740567396@gmail.com>
2023-05-05 08:37:16 +0800

    寻找重复元素

--- src/main/java/com/kuaishou/raymond/algorithm/leetcode/hot100/h17technique/Hot287FindTheDumplicatedNumber.java
+++ src/main/java/com/kuaishou/raymond/algorithm/leetcode/hot100/h17technique/Hot287FindTheDumplicatedNumber.java
@@ -9,6 +9,10 @@ import static com.kuaishou.raymond.algorithm.utils.AlgoUtils.swap;
  * CreateTime: 2023/5/4 19:03
  * 题目：<a href="https://leetcode.cn/problems/find-the-duplicate-number/?envType=study-plan-v2&id=top-100-liked">287. 寻找重复数</a>
  * 解析：<a href="https://leetcode.cn/problems/find-the-duplicate-number/solution/xun-zhao-zhong-fu-shu-by-leetcode-solution/">...</a>
+ * - 要求：不能修改数组，且只能使用 O(1) 的时间
+ * 相似题目：
+ * - {@link com.kuaishou.raymond.algorithm.leetcode.hot100.h7linkedlist.Hot141LinkedListCycle}
+ * - <a href="https://leetcode.cn/problems/linked-list-cycle-ii/solution/huan-xing-lian-biao-ii-by-leetcode/">142. 环形链表 II</a>
  */
 public class Hot287FindTheDumplicatedNumber {
 
@@ -24,21 +28,54 @@ public class Hot287FindTheDumplicatedNumber {
      * 当重复的数字 num 试图归位时，发现如果当前索引上的元素 num 已经与其相等，则表示找到了重复数字。
      */
     public static int findDuplicate(int[] nums) {
-        for (int i = 0; i < nums.length; i++) {
-            while (nums[i] != i) {
-                // 如果以当前值为索引的位置上的值已经正确归位，则找到了重复数字。
-                if (nums[nums[i]] == nums[i]) {
-                    return nums[i];
-                }
-                // 如果当前值未被正确放置到以其为下标的索引上，持续交换。
-                swap(nums, i, nums[i]);
+        while (true) {
+            // 如果以当前值为索引的位置上的值已经正确归位，则找到了重复数字。
+            if (nums[nums[0]] == nums[0]) {
+                return nums[0];
             }
+            // 如果当前值未被正确放置到以其为下标的索引上，持续交换。
+            swap(nums, 0, nums[0]);
         }
-        return -1;
     }
 
     /**
-     * 龟兔赛跑
+     * 二分搜索：统计每个数字出现的次数
+     * 题目保证了一定出现并只出现一个重复数字，假如取数组中点将数组分为前后两段，
+     * 如 [1,2,2,3,4], 取中点 nums[2]=2 将数组划分成 [1,2,2] 与 [3,4] 两段，
+     * - 在前半段统计 <= nums[2] 的数字的数量，即 1+1+1=3;
+     * - 在后半段统计 > nums[2] 的数字的数量，即 1+1=2;
+     * 而如果数字互不相同，如 [1,2,5,3,4]，以中点 nums[2] 按照相同方式统计，
+     * - 在前半段统计 <= nums[2] 的数字的数量，即 1+1=2;
+     * - 在后半段统计 > nums[2] 的数字的数量，即 1+1=2;
+     * 也就是说，前后两段中出现数字的数量是相同的，但如果出现了重复数字，
+     * 则出现重复数字的那段区间的数字 count 会多 1，就可以排除掉另一段区间的所有元素，实现"二分"效果。
+     */
+    public int findDuplicateIII(int[] nums) {
+        // 在 1..n 之间进行二分搜索
+        int left = 1;
+        int right = nums.length - 1;
+        while (left < right) {
+            int middle = (left + right) >>> 1;
+            int counter = 0;
+            for (int num : nums) {
+                if (num <= middle) {
+                    counter++;
+                }
+            }
+
+            if (counter > middle) {
+                // 某个数字至少出现了两次，并且是出现在前半段区间。
+                right = middle;
+            } else {
+                left = middle + 1;
+            }
+        }
+        return left;
+    }
+
+    /**
+     * 龟兔赛跑/弗洛伊德判圈法
+     * 本题最优解
      */
     public int findDuplicateII(int[] nums) {
         // Find the intersection point of the two runners.
